ca8e34991c894303b7a6fd4f0077e49c2aa892c0
RajivranjanV <rajivrockz94@gmail.com>
2022-07-23 16:46:32 +0530

    new code

---
+++ src/frd/org/ClassFriends.java
@@ -0,0 +1,19 @@
+package frd.org;
+
+public class ClassFriends {
+private void frdsClass() {
+	System.out.println("Ram");
+}
+private void rollNO() {
+	System.out.println("39");
+}
+private void batchClass() {
+	System.out.println("2013-2017");
+}
+public static void main(String[] args) {
+	ClassFriends onFriend=new ClassFriends();
+	onFriend.batchClass();
+	onFriend.rollNO();
+	onFriend.frdsClass();
+}
+}
